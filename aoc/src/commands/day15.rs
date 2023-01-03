da531ba6219c28838febe39a129f59d9a9802513
gnome-skillet <>
2023-01-03 10:38:52 -0800

    feat(coalesce): move coalesce logic into function
    move coalesce logic into function.

--- aoc/src/commands/day15.rs
+++ aoc/src/commands/day15.rs
@@ -6,8 +6,6 @@ use super::{CommandImpl, DynError};
 
 use crate::utils::slurp_file;
 
-use itertools::Itertools;
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 use std::cmp::Ordering;
@@ -15,11 +13,8 @@ use std::ops::{Deref, DerefMut, RangeInclusive};
 //use std::{collections::VecDeque, fmt::Display};
 
 use nom::{
-    branch::alt,
     bytes::complete::tag,
-    character::complete::multispace1,
-    multi::separated_list1,
-    sequence::{delimited, preceded},
+    sequence::{preceded},
     *,
 };
 
@@ -56,7 +51,6 @@ impl Point {
     }
 
     pub fn coverage(&self, beacon: &Self, row: i32) -> Option<BeaconExclusionZone> {
-        let mut covered: Vec<Point> = vec![];
         let perp_distance: u32 = self.perpendicular_distance(row);
         if perp_distance > self.distance(beacon) {
             return None;
@@ -78,7 +72,7 @@ pub struct BeaconExclusionZone(RangeInclusive<i32>);
 
 impl BeaconExclusionZone {
     fn new(start: i32, end: i32) -> BeaconExclusionZone {
-        BeaconExclusionZone((start..=end))
+        BeaconExclusionZone(start..=end)
     }
 
     pub fn overlaps(&self, other: &Self) -> bool {
@@ -139,21 +133,34 @@ fn read_line(input: &str) -> IResult<&str, (Point, Point)> {
 }
 
 pub fn coalesce_range(
-    this: &mut BeaconExclusionZone,
-    that: &mut BeaconExclusionZone,
-) -> BeaconExclusionZone {
-    BeaconExclusionZone::new(*this.start().min(that.start()), *this.end().max(that.end()))
+    mut ranges: Vec<BeaconExclusionZone>,
+) -> Vec<BeaconExclusionZone> {
+        let mut result: Vec<BeaconExclusionZone> = vec![];
+        let mut item: BeaconExclusionZone = ranges.pop().unwrap();
+        while !ranges.is_empty() {
+            let top: BeaconExclusionZone = ranges.pop().unwrap();
+            //println!("compare {:?} with {:?}", item, top);
+            if item.overlaps(&top) {
+                item = item.coalesce(&top);
+                //println!("coalesce {:?}", item);
+            } else {
+                result.push(top);
+            }
+        }
+        result.push(item);
+        result
 }
 
-impl CommandImpl for Day15 {
-    fn main(&self) -> Result<(), DynError> {
-        let lines: Vec<String> = slurp_file(&self.input)?;
-        let mut existing_beacons: HashSet<i32> = HashSet::new();
-        let mut ranges: Vec<BeaconExclusionZone> = vec![];
-        let row: i32 = 2;
+pub struct SearchSpace {
+    sensors_beacons: Vec<(Point, Point)>,
+    grid_range: RangeInclusive<i32>,
+}
+
+impl SearchSpace {
+    fn new(lines: Vec<String>) -> SearchSpace {
         let mut min_x: i32 = i32::MAX;
         let mut max_x: i32 = i32::MIN;
-
+        let mut sensors_beacons: Vec<(Point, Point)> = vec![];
         for line in lines {
             let x = read_line(&line);
             let (sensor, beacon) = x.unwrap().1;
@@ -161,40 +168,47 @@ impl CommandImpl for Day15 {
             min_x = min_x.min(smallest_x);
             let largest_x = sensor.x.max(beacon.x);
             max_x = max_x.max(largest_x);
+            sensors_beacons.push((sensor, beacon));
+        }
+        let grid_range: RangeInclusive<i32> = min_x..=max_x;
+        SearchSpace{ sensors_beacons, grid_range }
+    }
+
+    fn coverage(&self, row: i32) -> i32 {
+        //let row: i32 = 2000000;
+        //let row: i32 = 10;
+        let mut existing_beacons: HashSet<Point> = HashSet::new();
+        let mut ranges: Vec<BeaconExclusionZone> = vec![];
+        for (sensor, beacon) in &self.sensors_beacons {
             if sensor.distance(&beacon) >= sensor.perpendicular_distance(row) {
                 ranges.push(sensor.coverage(&beacon, row).unwrap());
             }
             // keep tally of existing beacons on row
             if beacon.perpendicular_distance(row) == 0 {
-                existing_beacons.insert(beacon.x);
+                existing_beacons.insert(*beacon);
             }
         }
         ranges.sort_by(|a, b| a.partial_cmp(b).unwrap());
         ranges.reverse();
-        let mut result: Vec<BeaconExclusionZone> = vec![];
-        let mut item: BeaconExclusionZone = ranges.pop().unwrap();
-        while !ranges.is_empty() {
-            let mut top: BeaconExclusionZone = ranges.pop().unwrap();
-            println!("compare {:?} with {:?}", item, top);
-            if item.overlaps(&top) {
-                item = item.coalesce(&top);
-                println!("coalesce {:?}", item);
-            } else {
-                //result.push(item);
-            }
-        }
-        //result.push(item);
-        println!("evaluate item ({:?})", item);
-        println!("evaluate ranges ({:?})", ranges);
-        //vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
-        println!("row length ({min_x}, {max_x})");
-        let grid_range: RangeInclusive<i32> = min_x..=max_x;
+        let ranges = coalesce_range(ranges);
+        let n_existing_beacons: i32 = existing_beacons.len() as i32;
+        let n_excluded_positions: i32 = ranges[0].end() - ranges[0].start() + 1 - n_existing_beacons;
+        let n_total_positions: i32 = self.grid_range.end() - self.grid_range.start();
+        println!("excluded positions {:?} out of {:?}", n_excluded_positions, n_total_positions);
+        n_excluded_positions
+    }
+}
 
-        println!("Range of row({row}) {:?}", grid_range);
-        println!("Covers {:?} grid before correction", existing_beacons.len());
-        println!("existing beacon {:?}", existing_beacons);
+impl CommandImpl for Day15 {
+    fn main(&self) -> Result<(), DynError> {
+        let lines: Vec<String> = slurp_file(&self.input)?;
+        //let row: i32 = 2000000;
+        let row: i32 = 10;
+
+        let search_space: SearchSpace = SearchSpace::new(lines);
+        let n: i32 = search_space.coverage(row);
+        println!("there are {n} excluded positions");
         //6275922
-        //println!("EX: {:?}", self.input);
         Ok(())
     }
 }
