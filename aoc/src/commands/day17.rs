d8324076e798a94f30f88ff888a6c43fe7f6ac9e
Carlo Cosenza <214245+gnome-skillet@users.noreply.github.com>
2023-08-14 09:16:39 -0700

    refactor(offset): use offset to handle overlap evaluation
    use offset to handle overlap evaluation between rock and rock pile

--- aoc/src/commands/day17.rs
+++ aoc/src/commands/day17.rs
@@ -35,116 +35,156 @@ impl JetDirection {
     }
 }
 
-pub trait Projectile {
-    fn can_move_down(&self) -> bool;
-    fn can_move_left(&self) -> bool;
-    fn can_move_right(&self) -> bool;
-    fn move_down(&mut self);
-    fn move_left(&mut self);
-    fn move_right(&mut self);
-    fn lowest_point(&self) -> u64;
-    fn highest_point(&self) -> u64;
-    fn shape(&self) -> &'static str;
-    fn display(&self);
-}
-
 #[derive(Debug)]
 pub struct Rock {
-    points: Vec<u8>,
-    offset: u64,
+    row: Vec<u8>,
+    offset: usize,
 }
 
-pub fn plank(offset: u64) -> Rock {
-    //println!("plank({:?})", start);
-    Rock { points: vec![30 as u8], offset: offset }
+pub fn plank(offset: usize) -> Rock {
+    Rock { row: vec![30_u8], offset }
 }
 
-pub fn cross(offset: u64) -> Rock {
-    //println!("cross({:?})", start);
-    Rock { points: vec![8u8, 28u8, 8u8], offset }
+pub fn cross(offset: usize) -> Rock {
+    Rock { row: vec![8_u8, 28_u8, 8_u8], offset }
 }
 
-pub fn ell(offset: u64) -> Rock {
-    //println!("ell({:?})", start);
-    Rock { points: vec![28u8, 4u8, 4u8], offset }
+pub fn ell(offset: usize) -> Rock {
+    Rock { row: vec![28_u8, 4_u8, 4_u8], offset }
 }
 
-pub fn pole(offset: u64) -> Rock {
-    //println!("pole({:?})", start);
-    Rock { points: vec![16u8, 16u8, 16u8, 16u8], offset }
+pub fn pole(offset: usize) -> Rock {
+    Rock { row: vec![16_u8, 16_u8, 16_u8, 16_u8], offset }
 }
 
-pub fn block(offset: u64) -> Rock {
-    //println!("block({:?})", start);
-    Rock { points: vec![24u8, 24u8], offset }
+pub fn block(offset: usize) -> Rock {
+    Rock { row: vec![24_u8, 24_u8], offset }
 }
 
-impl Projectile for Rock {
+// determine if the left shifted new block is disjoint with old block
+pub fn left_disjoint(shifted_block: u8, old_block: u8) -> bool {
+    disjoint(shifted_block << 1, old_block)
+}
+
+// determine if the right shifted new block is disjoint with old block
+pub fn right_disjoint(shifted_block: u8, old_block: u8) -> bool {
+    disjoint(shifted_block >> 1, old_block)
+}
+
+// determine if the new block is disjoint with the old block
+pub fn disjoint(this_block: u8, that_block: u8) -> bool {
+    this_block & that_block == 0
+}
+
+// combine shifted_block and old_block
+pub fn combine(shifted_block: usize, old_block: usize) -> usize {
+    shifted_block | old_block
+}
+
+impl Rock {
     fn move_down(&mut self) {
         self.offset -= 1;
     }
 
     fn move_left(&mut self) {
         if self.can_move_left() {
-            self.points.iter_mut().map(|x| *x <<= 1).collect::<Vec<_>>();
+            //self.row.iter_mut().map(|x| *x <<= 1).collect::<Vec<_>>();
+            for x in self.row.iter_mut() {
+                *x <<= 1;
+            }
         }
     }
 
     fn move_right(&mut self) {
-        self.points.iter_mut().map(|x| *x >>= 1).collect::<Vec<_>>();
+        if self.can_move_right() {
+            //self.row.iter_mut().map(|x| *x >>= 1).collect::<Vec<_>>();
+            for x in self.row.iter_mut() {
+                *x >>= 1;
+            }
+        }
     }
 
-    fn can_move_down(&self) -> bool {
+    fn has_hit_bottom(&self) -> bool {
         self.offset > 0
     }
 
     fn can_move_left(&self) -> bool {
-        self.points.iter().all(|p| p & 64u8 == 0)
+        self.row.iter().all(|p| p & 64u8 == 0)
     }
 
     fn can_move_right(&self) -> bool {
-        self.points.iter().all(|p| p & 1u8 == 0)
+        self.row.iter().all(|p| p & 1u8 == 0)
     }
 
-    fn highest_point(&self) -> u64 {
-        self.offset + self.points.len() as u64
+    fn highest_point(&self) -> usize {
+        self.offset + self.row.len() as usize - 1
     }
 
-    fn lowest_point(&self) -> u64 {
-        self.offset as u64
+    fn lowest_point(&self) -> usize {
+        self.offset as usize
     }
 
     fn shape(&self) -> &'static str {
-        //format!("rock {:?}", self.points).as_str()
+        //format!("rock {:?}", self.row).as_str()
         "rock"
     }
 
     fn display(&self) {
-        for point in self.points.iter().rev() {
+        println!("offset: {:?}", self.offset);
+        for point in self.row.iter().rev() {
             println!("{point:0>7b}");
         }
-        println!("");
+        println!();
+    }
+
+    fn offset(&self) -> usize {
+        self.offset
+    }
+
+    fn row(&self, row_index: usize) -> u8 {
+        println!("row: {row_index} - {:?}", self.offset);
+        let index = (row_index - self.offset);
+        self.row[index as usize]
     }
 }
 
 #[derive(Debug)]
 pub struct RockPile {
     rocks: Vec<u8>,
-    offset: u64,
+    offset: usize,
+    nrocks: usize,
 }
 
 impl RockPile {
     fn new() -> RockPile {
-        RockPile { rocks: vec![255u8, 1], offset: 0 }
+        RockPile { rocks: vec![255u8, 1], offset: 0, nrocks: 0 }
     }
 
-    // index of first rock formation above floor
-    fn tallest_point(&self) -> u64 {
-        self.offset + self.rocks.len() as u64 - 1
+    fn add_row(&mut self, row: usize, rock: u8) {
+        self.rocks[row] = rock;
+    }
+
+    // index of tallest rock above floor
+    fn highest_point(&self) -> usize {
+        if self.rocks.len() > 0 {
+            self.offset
+        } else {
+            self.offset + self.rocks.len() - 1
+        }
     }
 
     fn is_blocked(&self, rock: &Rock) -> bool {
-        rock.points.iter().any(|p| self.rocks.contains(p))
+        if rock.offset() > (self.highest_point() + 1) {
+            return false;
+        }
+        let rock_row: u8 = rock.row(1);
+        let rockpile_row: u8 = self.rocks[0];
+        //let highest_row: Vec<usize> = vec![self.highest_point(), rock.highest_point()];
+        //let lowest_high_point = highest_row.iter().min().unwrap();
+        //let j = 0;
+        //for i in rock.offset()..*lowest_high_point {}
+        //rock.row.iter().any(|p| self.rocks.contains(p))
+        !disjoint(rock_row, rockpile_row)
     }
 
     fn is_open(&self, rock: &Rock) -> bool {
@@ -155,18 +195,31 @@ impl RockPile {
         for point in self.rocks.iter().rev() {
             println!("{point:0>7b}");
         }
-        println!("");
+        println!();
     }
 
-    fn is_covered(&mut self, row: u64) -> bool {
-        //(0..7).into_iter().map(|x| (x, row)).all(|p| self.rocks.contains(&p))
-        todo!()
-    }
+    //fn is_covered(&mut self, row: usize) -> bool {
+    //(0..7).into_iter().map(|x| (x, row)).all(|p| self.rocks.contains(&p))
+    //    todo!()
+    //}
 
     fn collapse(&mut self, nlevels: usize) {
         assert!(self.rocks.len() > nlevels);
         self.rocks.drain(0..nlevels);
-        self.offset += nlevels as u64;
+        self.offset += nlevels as usize;
+    }
+
+    fn new_rock(&mut self, rock_shape: RockShape) -> Box<Rock> {
+        println!("new_rock({:?})", rock_shape);
+        rock_shape.projectile(self.highest_point() + 3)
+    }
+
+    fn add_rock_to_pile(&mut self, rock: &Rock) {
+        self.nrocks += 1;
+    }
+
+    fn rock_count(&self) -> usize {
+        self.nrocks
     }
 }
 
@@ -180,7 +233,7 @@ pub enum RockShape {
 }
 
 impl RockShape {
-    pub fn projectile(&self, start: u64) -> Box<dyn Projectile> {
+    pub fn projectile(&self, start: usize) -> Box<Rock> {
         match self {
             RockShape::Plank => Box::new(plank(start)),
             RockShape::Cross => Box::new(cross(start)),
@@ -196,74 +249,134 @@ fn parse_jets(input: &str) -> IResult<&str, Vec<char>> {
     Ok((input, vecs))
 }
 
-//const NUM_ROCKS: u64 = 2022;
-const NUM_ROCKS: u64 = 5;
-const ROCK_OFFSET: u64 = 4;
-//const NUM_ROCKS: u64 = 1000000000000;
-//const NUM_ROCKS: usize = 2;
+const NUM_ROCKS: usize = 5;
+const ROCK_OFFSET: usize = 3;
+//const NUM_ROCKS: usize = 2022;
+//const NUM_ROCKS: usize = 1000000000000;
+
+fn get_jetstream(path: &Utf8PathBuf) -> Vec<JetDirection> {
+    let characters: String = fs::read_to_string(path).unwrap();
+    let (_, jets) = parse_jets(&characters).unwrap();
+    jets.into_iter().map(JetDirection::direction).collect()
+}
 
 impl CommandImpl for Day17 {
     fn main(&self) -> Result<(), DynError> {
-        let characters: String = fs::read_to_string(&self.input).unwrap();
-        let (_, jets) = parse_jets(&characters).unwrap();
-        let jets: Vec<JetDirection> =
-            jets.into_iter().map(|x| JetDirection::direction(x)).collect();
-        let mut jet_iter = jets.iter().cycle();
+        let jets: Vec<JetDirection> = get_jetstream(&self.input);
+        let mut jetstream = jets.iter().cycle();
 
-        let mut n: u64 = 0;
         let mut rock_pile = RockPile::new();
-        //println!("rock_pile: {:?}", rock_pile);
-        let N_JETS: u64 = jets.len() as u64;
-        const N_SHAPES: u64 = 5;
-        println!("njets = {N_JETS}, n_shapes = {N_SHAPES}");
 
-        let mut njets: u64 = 0;
-        let mut nshapes: u64 = 0;
         for shape in RockShape::iter().cycle() {
-            let mut rock = shape.projectile(rock_pile.tallest_point());
+            let mut rock = rock_pile.new_rock(shape);
+            println!("init");
             rock.display();
-            nshapes += 1;
-            //println!("{:?}:{:?}", shape, rock.shape());
-            //rock.display();
             loop {
-                let jet = jet_iter.next();
-                nshapes += 1;
-                //print!("move {:?}", jet);
+                let jet = jetstream.next();
                 match *jet.unwrap() {
                     JetDirection::Left => {
-                        if rock.can_move_left() {
-                            rock.move_left();
-                            println!("left");
-                            rock.display();
-                        }
+                        println!("left");
+                        rock.move_left();
                     }
                     JetDirection::Right => {
-                        if rock.can_move_right() {
-                            rock.move_right();
-                            println!("right");
-                            rock.display();
-                        }
+                        println!("right");
+                        rock.move_right();
                     }
                     _ => println!("panic"),
                 }
-                //print!("move {j}");
-                //rock.display();
-                if rock.can_move_down() {
+                rock.display();
+                if !rock.has_hit_bottom() {
                     rock.move_down();
                 } else {
-                    //println!("break loop");
-                    //rock_pile.add(&mut rock);
+                    rock_pile.add_rock_to_pile(&rock);
                     break;
                 }
             }
 
-            n = n + 1;
-            if n == NUM_ROCKS {
+            if rock_pile.rock_count() == NUM_ROCKS {
                 break;
             }
         }
-        println!("top {:?}", rock_pile.tallest_point());
+        println!("top {:?}", rock_pile.highest_point());
+        let mut rock_pile = RockPile::new();
+        rock_pile.add_row(0, 63u8);
+        let rock = plank(1);
+        rock.display();
+        rock_pile.display();
+        let blocked: bool = rock_pile.is_blocked(&rock);
+        println!("blocked = {blocked}");
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn combine_works() {
+        let shifted_block = 1;
+        let old_block = 2;
+        assert_eq!(
+            combine(shifted_block, old_block),
+            3,
+            "Expect {shifted_block:0>7b} | {old_block:0>7b} = {:0>7b}",
+            3
+        );
+    }
+
+    #[test]
+    fn disjoint_works() {
+        let new_block = 1_u8;
+        let old_block = 63_u8;
+        assert!(
+            !disjoint(new_block, old_block),
+            "Expect {new_block:0>7b} and {old_block:0>7b} are not disjoint",
+        );
+    }
+
+    #[test]
+    fn left_disjoint_works() {
+        let shifted_block = 1;
+        let old_block = 2;
+        assert!(
+            !left_disjoint(shifted_block, old_block),
+            "Expect {shifted_block:0>7b} and {old_block:0>7b} are not left disjoint",
+        );
+        let shifted_block = 1;
+        let old_block = 4;
+        assert!(
+            left_disjoint(shifted_block, old_block),
+            "Expect {shifted_block:0>7b} and {old_block:0>7b} are left disjoint",
+        );
+    }
+
+    #[test]
+    fn right_disjoint_works() {
+        let shifted_block = 2;
+        let old_block = 1;
+        assert!(
+            !right_disjoint(shifted_block, old_block),
+            "Expect {shifted_block:0>7b} and {old_block:0>7b} are not right disjoint",
+        );
+        let shifted_block = 4;
+        let old_block = 1;
+        assert!(
+            right_disjoint(shifted_block, old_block),
+            "Expect {shifted_block:0>7b} and {old_block:0>7b} are right disjoint",
+        );
+    }
+
+    #[test]
+    fn rockpile_block_works() {
+        let mut rock_pile = RockPile::new();
+        rock_pile.add_row(0, 0u8);
+        let rock = plank(2);
+        assert!(!rock_pile.is_blocked(&rock), "Expect rockpile does not block rock",);
+
+        let rock = plank(1);
+        rock_pile.add_row(0, 63u8);
+        assert!(rock_pile.is_blocked(&rock), "Expect rockpile blocks rock",);
     }
 }
