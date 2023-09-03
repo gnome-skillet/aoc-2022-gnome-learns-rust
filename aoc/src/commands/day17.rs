fb13b73d981d75162045e79ee76ed76b32065d48
Carlo Cosenza <214245+gnome-skillet@users.noreply.github.com>
2023-09-02 21:50:16 -0700

    fix(cycle): bump cycle to 60 layers to improve answer
    bump cycle to 60 layers to improve answer. started with 32 which resulted in inaccurate answer for
    previous answer.

--- aoc/src/commands/day17.rs
+++ aoc/src/commands/day17.rs
@@ -7,10 +7,12 @@ use clap::Parser;
 use super::{CommandImpl, DynError};
 
 use std::collections::{HashMap, VecDeque};
+use std::iter::FromIterator;
 
 use nom::{character::complete::one_of, multi::many1, *};
 use strum::IntoEnumIterator; // 0.17.1
 use strum_macros::EnumIter; // 0.17.1
+use ethnum::U256;
 
 use env_logger::Env;
 
@@ -24,7 +26,6 @@ pub struct Day17 {
 enum JetDirection {
     Right,
     Left,
-    Down,
     Unknown,
 }
 
@@ -34,7 +35,6 @@ impl JetDirection {
         match c {
             '<' => JetDirection::Left,
             '>' => JetDirection::Right,
-            'v' => JetDirection::Down,
             _ => JetDirection::Unknown,
         }
     }
@@ -43,7 +43,6 @@ impl JetDirection {
         match *self {
             JetDirection::Left => "Left".to_string(),
             JetDirection::Right => "Right".to_string(),
-            JetDirection::Down => "Down".to_string(),
             _ => "Unknown".to_string(),
         }
     }
@@ -160,13 +159,13 @@ impl Rock {
 pub struct RockPile {
     rocks: VecDeque<u8>,
     offset: usize,
-    nrocks: usize,
+    total_rocks: usize,
 }
 
 impl RockPile {
     fn new() -> RockPile {
         let rocks: VecDeque<u8> = VecDeque::new();
-        RockPile { rocks, offset: 0, nrocks: 0 }
+        RockPile { rocks, offset: 0, total_rocks: 0 }
     }
 
     #[allow(dead_code)]
@@ -179,20 +178,16 @@ impl RockPile {
         self.rocks[row] = rock;
     }
 
-    fn top_of_pile(&self) -> Option<u64> {
-
+    fn top_of_pile(&self) -> Option<String> {
         // If the chamber is less than 8 levels tall, we can't take a skyline
-        if self.nlayers() < 8 {
+        if self.nlayers() < 60 {
             return None;
         }
 
-        // Take the top 8 levels of the chamber and fold them into a 64-bit integer.
-        let result = self
-            .rocks
-            .iter()
-            .rev()
-            .take(8)
-            .fold(0u64, |acc, byte| (acc << 8) | *byte as u64);
+        let part = Vec::from_iter(self.rocks.iter().rev().take(60).cloned());
+        // Take the top 8 levels of the chamber and fold them into a 128-bit integer.
+        let result = String::from_utf8(part).ok()?;
+            //self.rocks.iter().rev().take(32).fold(U256::MIN, |acc, byte| (acc << 32) | U256::from(*byte));
         Some(result)
     }
 
@@ -270,7 +265,7 @@ impl RockPile {
                 self.rocks.push_back(p);
             }
         }
-        self.nrocks += 1;
+        self.total_rocks += 1;
     }
 
     #[allow(dead_code)]
@@ -279,9 +274,10 @@ impl RockPile {
             self.rocks.iter().rev().map(|x| format!("{x:07b}")).collect::<Vec<String>>().join("\n");
         let string = string.replace("1", "#");
         let string = string.replace("0", ".");
-        let nrocks: usize = self.nrocks;
+        let total_rocks: usize = self.total_rocks;
         let height: usize = self.nlayers();
-        let msg = format!("After {nrocks} rocks, the tower of rocks will be {height} units tall");
+        let msg =
+            format!("After {total_rocks} rocks, the tower of rocks will be {height} units tall");
         println!("{}\n{}", msg, string);
     }
 
@@ -293,7 +289,7 @@ impl RockPile {
     fn add_rocks_to_pile(&mut self, jets: Vec<JetDirection>, total_rocks: usize) {
         let mut jetstream = jets.iter().cycle();
         let rocks = RockShape::iter().cycle();
-        let mut visited = HashMap::with_capacity(2048);
+        let mut visited = HashMap::with_capacity(3066);
         for shape in rocks {
             let mut rock = self.new_rock(&shape);
             loop {
@@ -318,25 +314,17 @@ impl RockPile {
                     if let Some(top) = self.top_of_pile() {
                         let stage = (top, shape.idx(), jet.expect("expect string").idx());
                         if let Some((prev_rocks, prev_height)) = visited.get(&stage) {
-                            // number of rocks added in each repeating cycle.
-                            let repeat_len: usize = self.nrocks - prev_rocks;
+                            let rocks_in_cycle: usize = self.total_rocks - prev_rocks;
+                            let cycle_height: usize = self.height() - prev_height;
+                            let ncycles: usize =
+                                    (total_rocks - self.total_rocks) / rocks_in_cycle;
 
-                            // The number of repeats left before we add the final rock.
-                            let repeats: usize = (total_rocks - self.nrocks) / repeat_len;
+                            self.total_rocks += rocks_in_cycle * ncycles;
+                            self.offset += ncycles * (cycle_height);
 
-                            // Add all the rocks in all the repeating cycles between here and the end.
-                            //println!("prev_rocks: {prev_rocks}, total_rocks: {total_rocks}, repeat_len: {repeat_len}, repeats: {repeats}");
-                            self.nrocks += repeat_len * repeats;
-
-                            // Add the chamber height of the cycle to the accumulated height
-                            //println!("repeats: {repeats}, height{:?}, prev_height: {prev_height}", self.height());
-                            //println!("add {:?} height", repeats * (self.height() - prev_height));
-                            self.offset += repeats * (self.height() - prev_height);
-
-                            // Clear the map of visited states. No need to repeat
                             visited.clear();
                         } else {
-                            visited.insert(stage, (self.nrocks, self.height()));
+                            visited.insert(stage, (self.total_rocks, self.height()));
                         }
                     }
                     break;
@@ -344,12 +332,13 @@ impl RockPile {
                     rock.move_down();
                 }
             }
-            if self.nrocks == total_rocks {
+            if self.total_rocks == total_rocks {
                 break;
             }
         }
     }
 
+    #[allow(dead_code)]
     fn highest_plateau(&mut self) -> Option<usize> {
         if let Some(index) = self.rocks.iter().rev().position(|c| *c == 127_u8) {
             return Some(self.rocks.len() - index);
@@ -431,10 +420,10 @@ impl CommandImpl for Day17 {
         let mut rock_pile = RockPile::new();
         rock_pile.add_rocks_to_pile(jets, 1_000_000_000_000);
         println!("the rock pile is {:?} tall", rock_pile.height());
-        let jets: Vec<JetDirection> = get_jetstream(&self.input);
-        let mut rock_pile = RockPile::new();
-        rock_pile.add_rocks_to_pile(jets, 2022);
-        println!("the rock pile is {:?} tall", rock_pile.height());
+        //let jets: Vec<JetDirection> = get_jetstream(&self.input);
+        //let mut rock_pile = RockPile::new();
+        //rock_pile.add_rocks_to_pile(jets, 2022);
+        //println!("the rock pile is {:?} tall", rock_pile.height());
 
         Ok(())
     }
@@ -458,19 +447,6 @@ mod tests {
         ];
         let mut rock_pile = RockPile::new();
         rock_pile.add_rocks_to_pile(jets, 2, false);
-
-        //let jets: Vec<JetDirection> = vec![
-        //    JetDirection::Left,
-        //    JetDirection::Left,
-        //    JetDirection::Left,
-        //    JetDirection::Left,
-        //    JetDirection::Right,
-        //    JetDirection::Right,
-        //    JetDirection::Right,
-        //    JetDirection::Right,
-        //];
-        //let mut rock_pile = RockPile::new();
-        //rock_pile.add_rocks_to_pile(jets, 3, false);
     }
 
     #[test]
