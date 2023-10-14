a8061dd324b58b65b54af8e8938d8133a0917a61
Carlo Cosenza <214245+gnome-skillet@users.noreply.github.com>
2023-10-14 00:55:58 -0700

    feat(day18): initial commit
    initial commit

--- aoc/src/commands/day18.rs
+++ aoc/src/commands/day18.rs
@@ -5,6 +5,8 @@ use glam::f32::Vec3;
 use itertools::Itertools;
 
 use super::{CommandImpl, DynError};
+use std::collections::{HashMap, HashSet};
+use std::iter::FromIterator;
 
 use std::path::PathBuf;
 
@@ -59,14 +61,88 @@ fn pairwise_comparison(cubes: &Vec<Vec3>) -> usize {
     cubes.iter().tuple_combinations().map(|(x, y)| connected(*x, *y)).sum()
 }
 
+fn smallest_cube(cubes: &Vec<Vec3>) -> Option<Vec3> {
+    let xmin: f32 = cubes.iter().map(|x| x.x).min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+    let ymin: f32 = cubes.iter().map(|x| x.y).min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+    let zmin: f32 = cubes.iter().map(|x| x.z).min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+    Some(Vec3::new(xmin - 1.0, ymin - 1.0, zmin - 1.0))
+}
+
+fn largest_cube(cubes: &Vec<Vec3>) -> Option<Vec3> {
+    let xmax: f32 = cubes.iter().map(|x| x.x).max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+    let ymax: f32 = cubes.iter().map(|x| x.y).max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+    let zmax: f32 = cubes.iter().map(|x| x.z).max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+    Some(Vec3::new(xmax + 1.0, ymax + 1.0, zmax + 1.0))
+}
+
+fn surface_area(cubes: &Vec<Vec3>) -> Option<usize> {
+    let nmatches: usize = pairwise_comparison(cubes);
+    let ncubes: usize = cubes.len();
+    Some(ncubes * 6 - nmatches * 2)
+}
+
+fn adjacent_air(cubes: &Vec<Vec3>) -> Option<HashMap<(i32, i32, i32), u8>> {
+    let cube_map: HashSet<(i32, i32, i32)> =
+        HashSet::from_iter(cubes.iter().map(|x| (x.x as i32, x.y as i32, x.z as i32)).clone());
+    println!("cube_map: {:?}", cube_map);
+    let mut left_air: Vec<(i32, i32, i32)> = cubes
+        .iter()
+        .map(|x| ((x.x - 1.0) as i32, x.y as i32, x.z as i32))
+        .filter(|x| !cube_map.contains(x))
+        .collect_vec();
+    let right_air: Vec<(i32, i32, i32)> = cubes
+        .iter()
+        .map(|x| ((x.x + 1.0) as i32, x.y as i32, x.z as i32))
+        .filter(|x| !cube_map.contains(x))
+        .collect_vec();
+    let front_air: Vec<(i32, i32, i32)> = cubes
+        .iter()
+        .map(|x| (x.x as i32, (x.y + 1.0) as i32, x.z as i32))
+        .filter(|x| !cube_map.contains(x))
+        .collect_vec();
+    let back_air: Vec<(i32, i32, i32)> = cubes
+        .iter()
+        .map(|x| (x.x as i32, (x.y - 1.0) as i32, x.z as i32))
+        .filter(|x| !cube_map.contains(x))
+        .collect_vec();
+    let top_air: Vec<(i32, i32, i32)> = cubes
+        .iter()
+        .map(|x| (x.x as i32, x.y as i32, (x.z + 1.0) as i32))
+        .filter(|x| !cube_map.contains(x))
+        .collect_vec();
+    let bottom_air: Vec<(i32, i32, i32)> = cubes
+        .iter()
+        .map(|x| (x.x as i32, x.y as i32, (x.z - 1.0) as i32))
+        .filter(|x| !cube_map.contains(x))
+        .collect_vec();
+    left_air.extend(&right_air);
+    left_air.extend(&front_air);
+    left_air.extend(&back_air);
+    left_air.extend(&top_air);
+    left_air.extend(&bottom_air);
+    let air_map: HashSet<(i32, i32, i32)> =
+        HashSet::from_iter(left_air.iter().map(|x| (x.0, x.1, x.2)).clone());
+    let mut air_map = air_map.iter().map(|x| (*x, 0)).collect::<HashMap<_, _>>();
+    for (key, value) in air_map {}
+    Some(air_map)
+}
+
 impl CommandImpl for Day18 {
     fn main(&self) -> Result<(), DynError> {
         let characters = fs::read_to_string(&self.input).unwrap();
         let (_, cubes) = parse_cubes(&characters).unwrap();
-        let nmatches: usize = pairwise_comparison(&cubes);
-        let ncubes: usize = cubes.len();
-        let nsurfaces = ncubes * 6 - nmatches * 2;
-        println!("there are {nmatches} matches across {ncubes} cubes leaving {nsurfaces} exposed surfaces");
+        let nsurfaces =
+            if let Some(nsurfaces) = surface_area(&cubes) { nsurfaces } else { todo!() };
+        println!("the obsidian has {nsurfaces} surfaces exposed to the air");
+        let min_obsidian =
+            if let Some(min_obsidian) = smallest_cube(&cubes) { min_obsidian } else { todo!() };
+        let max_obsidian =
+            if let Some(max_obsidian) = largest_cube(&cubes) { max_obsidian } else { todo!() };
+        let adjacent = if let Some(adjacent) = adjacent_air(&cubes) { adjacent } else { todo!() };
+
+        println!("the smallest point is {:?}", min_obsidian);
+        println!("the largest point is {:?}", max_obsidian);
+        println!("the adjacent cubes are {:?}", adjacent);
 
         Ok(())
     }
@@ -78,5 +154,5 @@ mod tests {
 
     //#[test]
     //fn string_compare_after_1_turn() {
-    //}
+    //}the
 }
